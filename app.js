39847c5571d8220c29f900b346fdcbec4b04ad59
EmadElbehisy <>
2021-07-26 06:03:36 +0000

    addeddd

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ while(NumberOfPic > 30)
   NumberOfPic =prompt("Please Enter Number Less Than 30 !!!");
 }
 
-
+function statement ()
+{
 if (FavBrowser == "Google Chrome")
 {
   document.write('<p style="text-align: center; color:gold;">' +'Your Favorite Browser is: '+ FavBrowser + '</p>');
@@ -48,3 +49,18 @@ else
 {
 document.write("None");
 }
+}
+statement();
+
+
+
+function rate()
+{
+  var stars = prompt("How Many Stars Do You Are Rate My Website");
+  document.write('<p style="text-align: center; color:gold;">' +'Your Rate is: '+ stars + '</p>')
+  for (var i = 0 ; i<stars ; i++)
+  {
+    document.write(' <img src="rate/r1.png" alt="star" width="33%" height="400px"> ' )
+  }
+}
+rate();
